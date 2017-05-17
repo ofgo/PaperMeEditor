c39ad43b6c5885b282355a29648a2a5c449bad23
zhongming <zhongming@mucfc.com>
2017-05-17 21:10:55 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # PaperMeEditor
 An powerful editor for writing papers
+
+
+## TODO
+
+* 论文保存格式
+* 版本管理
+* 编辑器
+* 预览
+* 生成PDF
---
+++ main.js
@@ -0,0 +1,39 @@
+const url                    = require('url')
+const path                   = require('path')
+const { app, BrowserWindow } = require('electron')
+
+let mainWindow
+
+function createMainWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+
+  mainWindow.webContents.openDevTools()
+
+  mainWindow.on('closed', () => {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createMainWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createMainWindow()
+  }
+})
+
